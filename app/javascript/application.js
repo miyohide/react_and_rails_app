ad0db21ecf5b5ff1b64434a38a6fd37e772b8382
Hidenori Miyoshi <miyohide@gmail.com>
2022-06-21 21:00:52 +0900

    run eslint fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,17 +1,17 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./components/App";
+import '@hotwired/turbo-rails';
+import './controllers';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container);
 
-document.addEventListener("DOMContentLoaded", () => {
-    root.render(
-        <StrictMode>
-            <App />
-        </StrictMode>
-    );
+document.addEventListener('DOMContentLoaded', () => {
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
 });
--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -1,39 +1,39 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 // イベントオブジェクトの配列を受け取り、ソート済みリストとしてレンダリングする
-const EventList = ({events}) => {
-    const renderEvents = (eventArray) => {
-        // 日付の降順でソートする
-        eventArray.sort((a, b) => new Date(b.event_date) - new Date(a.event_date));
+const EventList = ({ events }) => {
+  const renderEvents = (eventArray) => {
+    // 日付の降順でソートする
+    eventArray.sort((a, b) => new Date(b.event_date) - new Date(a.event_date));
 
-        return eventArray.map((event) => (
-            <li key={event.id}>
-                {event.event_date}
-                {'-'}
-                {event.event_type}
-            </li>
-        ));
-    };
+    return eventArray.map((event) => (
+      <li key={event.id}>
+        {event.event_date}
+        -
+        {event.event_type}
+      </li>
+    ));
+  };
 
-    return (
-        <section>
-            <h2>Events</h2>
-            <ul>{renderEvents(events)}</ul>
-        </section>
-    );
+  return (
+    <section>
+      <h2>Events</h2>
+      <ul>{renderEvents(events)}</ul>
+    </section>
+  );
 };
 
 EventList.propTypes = {
-    events: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.number,
-        event_type: PropTypes.string,
-        event_date: PropTypes.string,
-        title: PropTypes.string,
-        speaker: PropTypes.string,
-        host: PropTypes.string,
-        published: PropTypes.bool,
-    })).isRequired,
+  events: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    event_type: PropTypes.string,
+    event_date: PropTypes.string,
+    title: PropTypes.string,
+    speaker: PropTypes.string,
+    host: PropTypes.string,
+    published: PropTypes.bool,
+  })).isRequired,
 };
 
 export default EventList;
