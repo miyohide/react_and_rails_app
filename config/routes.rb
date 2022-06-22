52634917db7870fd3d2238fbf4e1dae850d4f41d
Hidenori Miyoshi <miyohide@gmail.com>
2022-06-22 20:45:03 +0900

    add react routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root to: "site#index"
+  root to: redirect("/events")
+
+  # Reactアプリで使うルーティングをRailsに伝える
+  get "events", to: "site#index"
+  get "events/new", to: "site#index"
+  get "events/:id", to: "site#index"
+  get "events/:id/edit", to: "site#index"
 
   namespace :api do
     resources :events, only: %i[index show create destroy update]
