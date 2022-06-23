f55c9d8c007ca931d2b7baa9d49fa27fc224f4fd
Hidenori Miyoshi <miyohide@gmail.com>
2022-06-23 20:33:09 +0900

    create Event component

---
+++ app/javascript/components/Event.js
@@ -0,0 +1,57 @@
+import React from "react";
+import {useParams} from "react-router-dom";
+import PropTypes from "prop-types";
+
+const Event = ({ events }) => {
+  // URL内の動的パラメータを含むオブジェクトにアクセスする
+  const { id } = useParams();
+  // 表示したいイベントを検索する
+  const event = events.find((e) => e.id === Number(id));
+
+  return (
+    <>
+      <h2>
+        {event.event_date}
+        {"-"}
+        {event.event_type}
+      </h2>
+      <ul>
+        <li>
+          <strong>Type:</strong> {event.event_type}
+        </li>
+        <li>
+          <strong>Date:</strong> {event.event_date}
+        </li>
+        <li>
+          <strong>Title:</strong> {event.title}
+        </li>
+        <li>
+          <strong>Speaker:</strong> {event.speaker}
+        </li>
+        <li>
+          <strong>Host:</strong> {event.host}
+        </li>
+        <li>
+          <strong>Published:</strong> {event.published ? "yes" : "no"}
+        </li>
+      </ul>
+    </>
+  );
+};
+
+Event.propTypes = {
+  events: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      event_type: PropTypes.string.isRequired,
+      event_date: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      speaker: PropTypes.string.isRequired,
+      host: PropTypes.string.isRequired,
+      published: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Event;
+
