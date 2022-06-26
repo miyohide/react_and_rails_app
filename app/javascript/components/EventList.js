7e5661f2ddd27e4e701d8cdc23ef08937d446984
Hidenori Miyoshi <miyohide@gmail.com>
2022-06-26 20:36:05 +0900

    add eventList class

--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -20,7 +20,7 @@ const EventList = ({ events }) => {
   };
 
   return (
-    <section>
+    <section className="eventList">
       <h2>Events</h2>
       <ul>{renderEvents(events)}</ul>
     </section>
