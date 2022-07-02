e200220f286e204c0950784472ec62b0de065ddd
Hidenori Miyoshi <miyohide@gmail.com>
2022-07-02 20:45:11 +0900

    implement addEvent method

--- app/javascript/components/Editor.js
+++ app/javascript/components/Editor.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import EventList from './EventList';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Event from "./Event";
 import EventForm from "./EventForm";
 
@@ -9,6 +9,7 @@ const Editor = () => {
   const [events, setEvents] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,6 +29,31 @@ const Editor = () => {
     fetchData();
   }, []);
 
+  // newEventオブジェクトを受け取り、新しいEventを作成するそのデータを用いて
+  // 新しいイベントを作成するリクエストをAPIに送信する
+  const addEvent = async (newEvent) => {
+    try {
+      const response = await window.fetch("/api/events", {
+        method: "POST",
+        body: JSON.stringify(newEvent),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) throw Error(response.statusText);
+      // リクエストが成功すると、新しく作成したイベントがステート内に保持されている
+      // イベント配列に追加されてUIを更新する
+      const savedEvent = await response.json();
+      const newEvents = [...events, savedEvent];
+      setEvents(newEvents);
+      window.alert("Event Added!");
+      navigate(`/events/${savedEvent.id}`);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <Header />
@@ -40,7 +66,7 @@ const Editor = () => {
             <EventList events={events} />
 
             <Routes>
-              <Route path="new" element={<EventForm />} />
+              <Route path="new" element={<EventForm onSave={addEvent} />} />
               <Route path=":id" element={<Event events={events} />} />
             </Routes>
           </>
