cfb06cc1240c270cba42eba1bc6b0e282fcbd5e1
Hidenori Miyoshi <miyohide@gmail.com>
2022-07-06 20:45:13 +0900

    implement updateEvent

--- app/javascript/components/Editor.js
+++ app/javascript/components/Editor.js
@@ -76,6 +76,34 @@ const Editor = () => {
     }
   };
 
+  const updateEvent = async (updatedEvent) => {
+    try {
+      const response = await window.fetch(
+        `/api/events/${updatedEvent.id}`,
+        {
+          method: "PUT",
+          body: JSON.stringify(updatedEvent),
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) throw Error(response.statusText);
+
+      const newEvents = events;
+      const idx = newEvents.findIndex((event) => event.id === updatedEvent.id);
+      newEvents[idx] = updatedEvent;
+      setEvents(newEvents);
+
+      success("Event Updated!");
+      navigate(`/events/${updatedEvent.id}`);
+    } catch (error) {
+      handleAjaxError(error);
+    }
+  };
+
   return (
     <>
       <Header />
