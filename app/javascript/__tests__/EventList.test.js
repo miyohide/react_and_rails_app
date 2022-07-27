8627326cca04bc3c2ee51618c4a3b76ba2525836
Hidenori Miyoshi <miyohide@gmail.com>
2022-07-27 20:05:55 +0900

    add multi events test

--- app/javascript/__tests__/EventList.test.js
+++ app/javascript/__tests__/EventList.test.js
@@ -35,4 +35,18 @@ describe('EventList', () => {
     // 与えたeventのevent_typeの値に紐づくaタグを探しURLが/events/+idとなっていることを確認する
     expect(screen.getByText(/event type/).closest('a')).toHaveAttribute('href', '/events/1');
   })
+
+  test('renders EventList component with events', () => {
+    render(
+      <Router>
+        <EventList events={[
+          { id: 1, event_type: 'event type1', event_date: '2022-01-01', title: 'event title1', speaker: 'event speaker1', host: 'event host1', published: true },
+          { id: 2, event_type: 'event type2', event_date: '2022-03-01', title: 'event title2', speaker: 'event speaker2', host: 'event host2', published: true },
+          { id: 3, event_type: 'event type3', event_date: '2021-12-01', title: 'event title3', speaker: 'event speaker3', host: 'event host3', published: true },
+        ]} />
+      </Router>
+    );
+    // 与えたeventのevent_typeの値に紐づくaタグを探しURLが/events/+idとなっていることを確認する
+    expect(screen.getByText(/event type1/).closest('a')).toHaveAttribute('href', '/events/1');
+  })
 });
