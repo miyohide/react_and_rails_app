77a0fa9ecda801d3f76ad605d852e00b565fc2ad
Hidenori Miyoshi <miyohide@gmail.com>
2022-07-28 21:32:57 +0900

    create event test

---
+++ app/javascript/__tests__/Event.test.js
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+import {render, screen} from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import Event from "../components/Event";
+import { MemoryRouter as Router } from "react-router-dom";
+
+describe('Event', () => {
+  test('renders Event component without events', () => {
+    render(
+      <Router>
+        <Event events={[]} onDelete={() => {}}/>
+      </Router>
+    );
+    expect(screen.getByText(/Event not found!/)).toBeInTheDocument();
+  })
+});
