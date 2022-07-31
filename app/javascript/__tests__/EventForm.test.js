374adcdacd96886ccf64d3062284718b0d7afa6e
Hidenori Miyoshi <miyohide@gmail.com>
2022-07-31 18:46:16 +0900

    create eventform test

---
+++ app/javascript/__tests__/EventForm.test.js
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+import {render, screen} from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import EventForm from "../components/EventForm";
+import { MemoryRouter } from "react-router-dom";
+
+describe('EventForm', () => {
+  test('renders EventForm component without events', () => {
+    render(
+      <MemoryRouter>
+        <EventForm events={[]} onSave={jest.fn()} />
+      </MemoryRouter>
+    );
+    expect(screen.getByText(/New Event/)).toBeInTheDocument();
+  })
+});
