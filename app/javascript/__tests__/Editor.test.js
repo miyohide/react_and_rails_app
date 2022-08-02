6003ec1891484d6b064a28690792eefcb88b1823
Hidenori Miyoshi <miyohide@gmail.com>
2022-08-02 20:48:38 +0900

    create editor test

---
+++ app/javascript/__tests__/Editor.test.js
@@ -0,0 +1,31 @@
+import Editor from "../components/Editor";
+import {render, screen} from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import { MemoryRouter } from "react-router-dom";
+import {act} from "react-dom/test-utils";
+
+test('renders Editor', async () => {
+  const dummyEvents = [
+    {id: 1, event_type: "event type1", event_date: "2022-01-01", title: "event title", speaker: "event speaker", host: "event host", published: true}
+  ]
+  // global.fetch = jest.fn(() => Promise.resolve(dummyEvents))
+  global.fetch = jest.fn(
+    () => {
+      return Promise.resolve({
+        ok: true,
+        json: () => {
+          return Promise.resolve(dummyEvents)
+        }
+        }
+      )
+    }
+  )
+  await act(async () => {
+    render(
+      <MemoryRouter>
+        <Editor />
+      </MemoryRouter>
+    );
+  });
+  expect(screen.getByText(/event type1/)).toBeInTheDocument();
+})
