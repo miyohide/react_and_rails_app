03a1526f3639753173809b21cbcefe5f84b34381
Hidenori Miyoshi <miyohide@gmail.com>
2022-08-03 20:23:19 +0900

    run format

--- app/javascript/__tests__/Editor.test.js
+++ app/javascript/__tests__/Editor.test.js
@@ -1,21 +1,29 @@
 import Editor from "../components/Editor";
 import {render, screen} from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
-import { MemoryRouter } from "react-router-dom";
+import {MemoryRouter} from "react-router-dom";
 import {act} from "react-dom/test-utils";
 
 test('renders Editor', async () => {
   const dummyEvents = [
-    {id: 1, event_type: "event type1", event_date: "2022-01-01", title: "event title", speaker: "event speaker", host: "event host", published: true}
+    {
+      id: 1,
+      event_type: "event type1",
+      event_date: "2022-01-01",
+      title: "event title",
+      speaker: "event speaker",
+      host: "event host",
+      published: true
+    }
   ]
   // global.fetch = jest.fn(() => Promise.resolve(dummyEvents))
   global.fetch = jest.fn(
     () => {
       return Promise.resolve({
-        ok: true,
-        json: () => {
-          return Promise.resolve(dummyEvents)
-        }
+          ok: true,
+          json: () => {
+            return Promise.resolve(dummyEvents)
+          }
         }
       )
     }
@@ -23,7 +31,7 @@ test('renders Editor', async () => {
   await act(async () => {
     render(
       <MemoryRouter>
-        <Editor />
+        <Editor/>
       </MemoryRouter>
     );
   });
