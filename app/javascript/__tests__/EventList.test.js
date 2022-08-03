3354ca093ddf3f2394daca46a12f5ce8c3d5b110
Hidenori Miyoshi <miyohide@gmail.com>
2022-08-04 06:30:31 +0900

    add updateSearchTerm test

--- app/javascript/__tests__/EventList.test.js
+++ app/javascript/__tests__/EventList.test.js
@@ -1,4 +1,4 @@
-import {render, screen} from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
 import EventList from "../components/EventList";
 import { MemoryRouter as Router } from "react-router-dom";
@@ -51,5 +51,27 @@ describe('EventList', () => {
     expect(atags[1]).toHaveAttribute('href', '/events/2');
     expect(atags[2]).toHaveAttribute('href', '/events/1');
     expect(atags[3]).toHaveAttribute('href', '/events/3');
+  })
+
+
+  test('renders EventList when enter search text',async () => {
+    render(
+      <Router>
+        <EventList events={[
+          { id: 1, event_type: 'event type search1', event_date: '2022-01-01', title: 'event title1', speaker: 'event speaker1', host: 'event host1', published: true },
+          { id: 2, event_type: 'event type2', event_date: '2022-03-01', title: 'event title2', speaker: 'event speaker2', host: 'event host2', published: true },
+          { id: 3, event_type: 'event type search2', event_date: '2021-12-01', title: 'event title3', speaker: 'event speaker3', host: 'event host3', published: true },
+        ]} />
+      </Router>
+    );
+    // すべてのeventが描画されるのを待つ
+    await screen.findByText(/event type2/);
+    // テキストボックスに検索キーワードを入れる
+    fireEvent.keyUp(screen.getByRole('textbox'), {
+      target: { value: 'search' },
+    });
+
+    // 検索ワードの対象外であるevent type2のデータは無くなっていること
+    expect(screen.queryByText(/event type2/)).toBeNull();
   })
 });
